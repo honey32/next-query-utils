aad39dd55786f1cb0cdc4bdfca421fb4f398d729
Mitsu Takeuchi <mitsu.take.gemini@gmail.com>
2022-06-01 22:18:32 +0900

    fix: exported name of `getSingleQueryParam`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { getSingleQueryParam as getSingleQuery } from "./getSingleQueryParam";
+export { getSingleQueryParam } from "./getSingleQueryParam";
 export { removeQueryParams } from "./removeQueryParams";
 export { QueryMutation, queryMutation } from "./queryMutation";
 export { resetQuery, ResetQueryOptions } from "./resetQuery";
