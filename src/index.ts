d52e4f9b1403c07e68860c07ef7ee61b42922b5a
honey32 <honey32@bearwapps.com>
2022-06-14 23:01:42 +0900

    feat: implemented `setQueryParams`

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { getSingleQueryParam } from "./getSingleQueryParam";
 export { resetQuery, type ResetQueryOptions } from "./resetQuery";
 export { removeQueryParams } from "./removeQueryParams";
 export { queryMutation, type QueryMutation } from "./queryMutation";
+export { setQueryParams } from "./setQueryParams";
