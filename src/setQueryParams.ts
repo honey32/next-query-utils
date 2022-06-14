ad83f32e8fa05a4649660bec681e71596be9d221
honey32 <honey32@bearwapps.com>
2022-06-14 23:06:50 +0900

    feat: implemented `setQueryParams` (#24)

---
+++ src/setQueryParams.ts
@@ -0,0 +1,41 @@
+import { queryMutation } from "./queryMutation";
+
+type SetQueryParamsRHV = Record<
+  string,
+  string | string[] | undefined | null | 0 | false
+>;
+
+/**
+ * Sets values of query parameters.
+ *
+ * for each key in `params` object,
+ *
+ * - if value is *falsy*
+ *   ... resets the values for the key.
+ * - *otherwise*
+ *   ... replace old values for the key
+ *       with them with the given value
+ *
+ * @example
+ * ```
+ * const query = { a: "aaa" }
+ *
+ * setQueryParams({ a: "abc" })(query) // -> { a: "abc" }
+ * setQueryParams({ b: "bbb" })(query) // -> { a: "aaa", b: "bbb" }
+ * setQueryParams({})(query) // -> { a: "aaa" }
+ *
+ * // falsy values like: 0 | null | undefined | false | ""
+ * setQueryParams({ a: null })(query) // -> { a: [] }
+ * ```
+ */
+export const setQueryParams = (params: SetQueryParamsRHV) => {
+  return queryMutation((query) =>
+    Object.entries(params).reduce(
+      (acc, [key, value]) => ({
+        ...acc,
+        [key]: !value ? [] : value,
+      }),
+      query
+    )
+  );
+};
