746d368227b7b5bc95b3dd4a11c723fdeddc8001
honey32 <honey32@bearwapps.com>
2024-01-06 20:49:45 +0900

    fix: export `getMultipleQueryParamsCurried`

--- src/getters/index.ts
+++ src/getters/index.ts
@@ -1,3 +1,4 @@
 export { getMultipleQueryParams } from "./getMultipleQueryParams";
+export { getMultipleQueryParamsCurried } from "./getMultipleQueryParamsCurried";
 export { getSingleQueryParam } from "./getSingleQueryParam";
 export { getSingleQueryParamCurried } from "./getSingleQueryParamCurried";
