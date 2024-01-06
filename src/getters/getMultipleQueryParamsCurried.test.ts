ebc41ac45915e5cc25cdbb44ce72cd6ee10cc523
honey32 <honey32@bearwapps.com>
2024-01-06 18:41:45 +0900

    feat: getMultipleQueryParamsCurreid

---
+++ src/getters/getMultipleQueryParamsCurried.test.ts
@@ -0,0 +1,43 @@
+import { getMultipleQueryParams } from "./getMultipleQueryParams";
+import { ParsedUrlQuery } from "../types/ParsedUrlQuery";
+import { getMultipleQueryParamsCurried } from "./getMultipleQueryParamsCurried";
+
+describe("getMultipleQueryParamsCurried(key, pred)(query)", () => {
+  type Case = [query: ParsedUrlQuery];
+
+  it.each<Case>([[{ key: "a" }], [{ key: [] }], [{}]])(
+    '("key")(%p) matches non-curreid version\'s result',
+    (query) => {
+      const getKeys = getMultipleQueryParamsCurried("key");
+      expect(getKeys(query)).toEqual(getMultipleQueryParams(query, "key"));
+    },
+  );
+
+  it.each<Case>([
+    [{ key: "a" }],
+    [{ key: ["a"] }],
+    [{ key: ["a", "b"] }],
+    [{ key: ["b", "a"] }],
+    [{}],
+    [{ key: "b" }],
+    [{ key: ["b"] }],
+    [{ key: ["b", "c"] }],
+  ])(
+    '("key", (s) => s === "a")(%p) matches non-curreid version\'s result',
+    (query) => {
+      const result = getMultipleQueryParams(query, "key", (s) => s === "a");
+      const getKeysLoose = getMultipleQueryParamsCurried(
+        "key",
+        (s) => s === "a",
+      );
+      expect(getKeysLoose(query)).toEqual(result);
+
+      // strictly typed
+      const getKeysStrict = getMultipleQueryParamsCurried(
+        "key",
+        (s): s is "a" => s === "a",
+      );
+      expect<"a"[]>(getKeysStrict(query)).toEqual(result);
+    },
+  );
+});
