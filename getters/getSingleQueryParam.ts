76b6d5482d08b98d6dbbf78d07165fe2454fc34f
honey32 <honey32@bearwapps.com>
2024-07-20 21:48:30 +0900

    docs: overloads

--- getters/getSingleQueryParam.ts
+++ getters/getSingleQueryParam.ts
@@ -1,15 +1,34 @@
 import type { ParsedUrlQuery } from "../types/ParsedUrlQuery.ts";
 
+/**
+ * Returns the first value for the specified *key* in the query object.
+ * If `pred` specified, returns the first value *that meets it*.
+ *
+ * ---
+ *
+ * 返り値は、指定した `key` に対応するパラメータのうち最初の値。
+ * もし `pred` が指定されている場合は、**その関数に合格した**値のうち最初のものが返る。
+ *
+ * @example
+ * ```
+ * getSingleQueryParam({},            "id") === undefined
+ * getSingleQueryParam({ id: "aaa" }, "id") === "aaa"
+ *
+ * // with pred specified
+ * const is_a = (s: string) => s === "a"
+ * getSingleQueryParam({ id: "a" },        "id", is_a) === "a"
+ * getSingleQueryParam({},                 "id", is_a) === undefined
+ * getSingleQueryParam({ id: "b" },        "id", is_a) === undefined
+ * getSingleQueryParam({ id: ["b", "a"] }, "id", is_a) === "a"
+ * ```
+ *
+ * @param pred *optional*. the first value *that fits this predicate* will be returned.
+ */
 export function getSingleQueryParam<T extends string>(
   query: ParsedUrlQuery,
   key: string,
   pred: (s: string) => s is T,
 ): T | undefined;
-export function getSingleQueryParam(
-  query: ParsedUrlQuery,
-  key: string,
-  pred?: (s: string) => boolean,
-): string | undefined;
 
 /**
  * Returns the first value for the specified *key* in the query object.
@@ -35,6 +54,12 @@ export function getSingleQueryParam(
  *
  * @param pred *optional*. the first value *that fits this predicate* will be returned.
  */
+export function getSingleQueryParam(
+  query: ParsedUrlQuery,
+  key: string,
+  pred?: (s: string) => boolean,
+): string | undefined;
+
 export function getSingleQueryParam(
   query: ParsedUrlQuery,
   key: string,
--- getters/getSingleQueryParamCurried.ts
+++ getters/getSingleQueryParamCurried.ts
@@ -1,15 +1,19 @@
 import type { ParsedUrlQuery } from "../types/ParsedUrlQuery.ts";
 import { getSingleQueryParam } from "./getSingleQueryParam.ts";
 
+/**
+ * "data-last" Curried version of {@link getSingleQueryParam}.
+ *
+ * @example
+ * ```
+ * const isValidSortOrder = (input: string): input is "asc" | "desc" => input === "asc" || input === "desc"
+ * const getSingleSortOrder = getSingleQueryParamCurreid("order", isValidSortOrder)
+ * ```
+ */
 export function getSingleQueryParamCurried<T extends string>(
   key: string,
   pred: (s: string) => s is T,
 ): (query: ParsedUrlQuery) => T | undefined;
-
-export function getSingleQueryParamCurried(
-  key: string,
-  pred?: (s: string) => boolean,
-): (query: ParsedUrlQuery) => string | undefined;
 
 /**
  * "data-last" Curried version of {@link getSingleQueryParam}.
@@ -20,6 +24,11 @@ export function getSingleQueryParamCurried(
  * const getSingleSortOrder = getSingleQueryParamCurreid("order", isValidSortOrder)
  * ```
  */
+export function getSingleQueryParamCurried(
+  key: string,
+  pred?: (s: string) => boolean,
+): (query: ParsedUrlQuery) => string | undefined;
+
 export function getSingleQueryParamCurried(
   key: string,
   pred?: (s: string) => boolean,
